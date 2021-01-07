da976636e42a4025694e57597cba062028d8240d
Andre Bellmann <andre.bellmann@huk-coburg.de>
2021-01-07 15:38:13 +0100

    try navigationbar color

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:coronaampel/controller/theme_controller.dart';
 import 'package:coronaampel/screens/search_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import './screens/tabs_screen.dart';
@@ -8,6 +9,11 @@ import './screens/city_detail_screen.dart';
 
 void main() async {
   await GetStorage.init();
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    systemNavigationBarColor: Colors.transparent, // navigation bar color
+    systemNavigationBarDividerColor:
+        Colors.transparent, //Navigation bar divider color
+  ));
   runApp(MyApp());
 }
 
