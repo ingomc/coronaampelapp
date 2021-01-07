56acc544f43ac5bbf8e6040eac01ed9e3d3691b1
Andre Bellmann <andre.bellmann@huk-coburg.de>
2021-01-07 14:22:11 +0100

    remove consolelogs

--- lib/services/remote_services.dart
+++ lib/services/remote_services.dart
@@ -7,20 +7,24 @@ class RemoteServices {
 
   static Future<Citys> fetchUsers([citys]) async {
     if (citys != null && citys.length > 0) {
-      citys.forEach((city) => {print(city.county)});
-
+      // Make list map with all locationstrings
       var locations = [];
       citys.forEach((city) => {
             locations.add("county = '${city.county.toString().toUpperCase()}'")
           });
-      print(Uri.encodeFull(locations.join(' OR ')));
+
+      // join all locations encode it
       String citysString = Uri.encodeFull(locations.join(' OR '));
 
+      // Real url fetched from RKI
+      // Cityname, incidences etc.
       String url =
           'https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?outFields=OBJECTID%2CGEN%2CBEZ%2CRS%2Ccases7_per_100k%2Clast_update&returnGeometry=false&f=json&outSR=4326&where=${citysString}';
-      print(url);
+      // print(url);
+
       var response = await client.get(url);
       if (response.statusCode == 200) {
+        // Succesful fetch
         var jsonString = response.body;
         var allData = citysFromJson(jsonString);
         return allData;
@@ -33,7 +37,6 @@ class RemoteServices {
       }
     }
     // Snackbar, no Citys selected!
-    // Snackbar
     Get.snackbar('Keine Landkreise ausgewählt',
         'Gehe auf die Startseite, Suche dort nach Landkreisen und füge diese deiner List hinzu!',
         snackPosition: SnackPosition.BOTTOM);
