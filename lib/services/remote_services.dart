ae3661040452fdc434ec41741e4db81b45eb24c7
Andre Bellmann <andre.bellmann@huk-coburg.de>
2021-01-07 11:45:19 +0100

    fetch multiple citys

--- lib/services/remote_services.dart
+++ lib/services/remote_services.dart
@@ -6,7 +6,7 @@ class RemoteServices {
 
   static Future<Citys> fetchUsers() async {
     var response = await client.get(
-        'https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?outFields=OBJECTID%2CGEN%2CBEZ%2CRS%2Ccases7_bl_per_100k%2Clast_update&returnGeometry=false&f=json&outSR=4326&where=county%20=%20%27SK%20COBURG%27');
+        'https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?outFields=OBJECTID%2CGEN%2CBEZ%2CRS%2Ccases7_bl_per_100k%2Clast_update&returnGeometry=false&f=json&outSR=4326&where=county%20=%20%27SK%20COBURG%27%20OR%20county%20=%20%27LK%20RH%C3%96N-GRABFELD%27');
     // var response = await client.get('https://jsonplaceholder.typicode.com/users');
 
     if (response.statusCode == 200) {
