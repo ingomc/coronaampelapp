e41e7c24625366b8fa8287f48d27f5c4591b4368
Andre Bellmann <andre.bellmann@huk-coburg.de>
2021-01-31 18:39:07 +0100

    move vaccinewidget in own file

---
+++ lib/widgets/vaccine_state_card.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
+
+class VaccineStateCard extends StatelessWidget {
+  const VaccineStateCard({
+    Key key,
+    @required this.state,
+    @required this.progress,
+    @required this.daysLeft,
+    @required this.vaccinated,
+    @required this.today,
+    @required this.target,
+    this.label = false,
+  }) : super(key: key);
+
+  final String state;
+  final double progress;
+  final int daysLeft;
+  final int vaccinated;
+  final int today;
+  final int target;
+  final bool label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: [
+          ListTile(
+            leading: CircleAvatar(
+              backgroundImage: ExactAssetImage('assets/states/10.png'),
+            ),
+            trailing: Text(
+              'Noch ${NumberFormat.decimalPattern('de-DE').format(daysLeft)} \nTage',
+              textAlign: TextAlign.right,
+            ),
+            title: Text(
+              '$state',
+              softWrap: false,
+              overflow: TextOverflow.fade,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            subtitle: Text(
+                'Impstatus: ${NumberFormat.decimalPattern('de-DE').format(progress)} %'),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            child: LinearPercentIndicator(
+              animation: true,
+              lineHeight: 10,
+              percent: progress / 100,
+              linearStrokeCap: LinearStrokeCap.roundAll,
+              backgroundColor: Theme.of(context).primaryColor,
+              progressColor: Colors.green,
+              trailing: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 4),
+                child:
+                    Text('70%', style: TextStyle(fontWeight: FontWeight.bold)),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    '${NumberFormat.compact(locale: 'de-DE').format(vaccinated)} verabreicht \n(+${NumberFormat.decimalPattern('de-DE').format(today)})',
+                  ),
+                ),
+                Text(
+                  '${NumberFormat.compact(locale: 'de-DE').format(target)}',
+                  textAlign: TextAlign.right,
+                ),
+              ],
+            ),
+          ),
+          if (label == true)
+            Container(
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(0, 16, 16, 0),
+                child: Text(
+                  'je 2 Impfdosen für 70% aller \nEinwohner*innen',
+                  textAlign: TextAlign.right,
+                ),
+              ),
+            ),
+          SizedBox(
+            height: 16,
+          )
+        ],
+      ),
+    );
+  }
+}
