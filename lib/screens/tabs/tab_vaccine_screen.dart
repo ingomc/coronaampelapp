b823515825491967d0966603f5a1fc0d112a81be
Andre Bellmann <andre.bellmann@huk-coburg.de>
2021-01-31 15:30:52 +0100

    content in vaccine

--- lib/screens/tabs/tab_vaccine_screen.dart
+++ lib/screens/tabs/tab_vaccine_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 
 class TabVaccineScreen extends StatelessWidget {
@@ -29,6 +31,10 @@ class TabVaccineScreen extends StatelessWidget {
                               backgroundImage:
                                   ExactAssetImage('assets/states/10.png'),
                             ),
+                            trailing: Text(
+                              'Noch 1.345 \nTage',
+                              textAlign: TextAlign.right,
+                            ),
                             title: Text(
                               'Bundesland',
                               softWrap: false,
@@ -38,24 +44,43 @@ class TabVaccineScreen extends StatelessWidget {
                             subtitle: Text('Impstatus: 3,1%'),
                           ),
                           Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 16),
+                            padding: const EdgeInsets.symmetric(horizontal: 12),
                             child: LinearPercentIndicator(
-                              lineHeight: 20,
+                              lineHeight: 10,
                               percent: 0.011,
+                              linearStrokeCap: LinearStrokeCap.roundAll,
                               backgroundColor: Theme.of(context).primaryColor,
                               progressColor: Colors.green,
                               trailing: Padding(
-                                padding: const EdgeInsets.only(left: 8.0),
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 4),
                                 child: Text('70%',
                                     style:
                                         TextStyle(fontWeight: FontWeight.bold)),
                               ),
                             ),
                           ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 16, vertical: 12),
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  child: Text(
+                                    '1.543.456 verabreicht',
+                                  ),
+                                ),
+                                Text(
+                                  '116,4 Mio',
+                                  textAlign: TextAlign.right,
+                                ),
+                              ],
+                            ),
+                          ),
                           Container(
                             width: double.infinity,
                             child: Padding(
-                              padding: const EdgeInsets.all(16.0),
+                              padding: const EdgeInsets.fromLTRB(0, 16, 16, 0),
                               child: Text(
                                 'je 2 Impfdosen für 70% aller \nEinwohner*innen',
                                 textAlign: TextAlign.right,
@@ -66,6 +91,61 @@ class TabVaccineScreen extends StatelessWidget {
                             height: 16,
                           )
                         ],
+                      ),
+                    ),
+
+                    //Timer
+                    GestureDetector(
+                      onTap: () {
+                        Get.defaultDialog(
+                          title: 'Hinweis',
+                          content: Text(
+                              'Diese Berechnung beruht auf der Annahme, dass eine Herdenimmunität dann erreicht ist, wenn mind. 70% der Einwohner*innen mit jeweils 2 Impfdosen gegen das Virus immunisiert wurden. \nHinweis: Zum aktuellen Zeitpunkt ist das reine Theorie!'),
+                          textConfirm: 'Ok',
+                          onConfirm: () {
+                            Get.back();
+                          },
+                        );
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(right: 16.0),
+                              child: Icon(
+                                MdiIcons.calendarClock,
+                                size: 36,
+                              ),
+                            ),
+                            Expanded(
+                              child: Text.rich(
+                                TextSpan(
+                                  text: 'Noch ',
+                                  children: <InlineSpan>[
+                                    TextSpan(
+                                      text: 'XX Tage',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    TextSpan(
+                                      text:
+                                          ' bis zur Herdenimmunität in Deutschland ',
+                                    ),
+                                    WidgetSpan(
+                                      baseline: TextBaseline.alphabetic,
+                                      alignment: PlaceholderAlignment.middle,
+                                      child: Icon(
+                                        Icons.info_outline,
+                                        size: 16,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ],
