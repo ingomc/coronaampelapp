44073986142951a05fc46bb11de58ee2c7ae2436
Andre Bellmann <andre.bellmann@huk-coburg.de>
2021-01-31 19:05:59 +0100

    nicer ui herden

--- lib/screens/tabs/tab_vaccine_screen.dart
+++ lib/screens/tabs/tab_vaccine_screen.dart
@@ -1,3 +1,4 @@
+import 'package:coronaampel/widgets/update_line.dart';
 import 'package:coronaampel/widgets/vaccine_state_card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -20,8 +21,9 @@ class TabVaccineScreen extends StatelessWidget {
               itemBuilder: (context, i) {
                 return Column(
                   children: [
-                    Center(
-                      child: Text('💉'),
+                    UpdateLine(
+                      left: ' XX:XX Uhr',
+                      right: 'Stand: XX:XX ',
                     ),
                     VaccineStateCard(
                       state: 'Bundesland!',
@@ -34,56 +36,59 @@ class TabVaccineScreen extends StatelessWidget {
                     ),
 
                     //Timer
-                    InkWell(
-                      onTap: () {
-                        Get.defaultDialog(
-                          title: 'Hinweis',
-                          content: Text(
-                              'Diese Berechnung beruht auf der Annahme, dass eine Herdenimmunität dann erreicht ist, wenn mind. 70% der Einwohner*innen mit jeweils 2 Impfdosen gegen das Virus immunisiert wurden. \nHinweis: Zum aktuellen Zeitpunkt ist das reine Theorie!'),
-                          textConfirm: 'Ok',
-                          onConfirm: () {
-                            Get.back();
-                          },
-                        );
-                      },
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Row(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(right: 16.0),
-                              child: Icon(
-                                MdiIcons.calendarClock,
-                                size: 36,
-                              ),
-                            ),
-                            Expanded(
-                              child: Text.rich(
-                                TextSpan(
-                                  text: 'Noch ',
-                                  children: <InlineSpan>[
-                                    TextSpan(
-                                      text: 'XX Tage',
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                    TextSpan(
-                                      text:
-                                          ' bis zur Herdenimmunität in Deutschland ',
-                                    ),
-                                    WidgetSpan(
-                                      baseline: TextBaseline.alphabetic,
-                                      alignment: PlaceholderAlignment.middle,
-                                      child: Icon(
-                                        Icons.info_outline,
-                                        size: 16,
-                                      ),
-                                    ),
-                                  ],
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      child: InkWell(
+                        onTap: () {
+                          Get.defaultDialog(
+                            title: 'Hinweis',
+                            content: Text(
+                                'In den letzten 7 Tagen wurden durchschnittlich XX Impfdosen verabreicht. Aus dieser Zahl berechne ich den wahrscheinlichen Zeitpunkt an dem die Herdenimmunität erreicht ist. Die Annahme ist, dass eine Herdenimmunität dann erreicht ist, wenn mind. 70% der Einwohner*innen mit jeweils 2 Impfdosen gegen das Virus immunisiert wurden. \nHinweis: Zum aktuellen Zeitpunkt ist das reine Theorie!'),
+                            textConfirm: 'Ok',
+                            onConfirm: () {
+                              Get.back();
+                            },
+                          );
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Row(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(right: 16.0),
+                                child: Icon(
+                                  MdiIcons.calendarClock,
+                                  size: 36,
                                 ),
                               ),
-                            ),
-                          ],
+                              Expanded(
+                                child: Text.rich(
+                                  TextSpan(
+                                    text: 'Noch ',
+                                    children: <InlineSpan>[
+                                      TextSpan(
+                                        text: 'XX Tage',
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      TextSpan(
+                                        text:
+                                            ' bis zur Herdenimmunität in Deutschland ',
+                                      ),
+                                      WidgetSpan(
+                                        baseline: TextBaseline.alphabetic,
+                                        alignment: PlaceholderAlignment.middle,
+                                        child: Icon(
+                                          Icons.info_outline,
+                                          size: 16,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
