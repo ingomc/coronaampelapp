18db205df51c63e70f101936d52c312d4c67ab08
Andre Bellmann <andre.bellmann@huk-coburg.de>
2021-02-23 00:40:21 +0100

    rewarded ad first milestone ready

---
+++ lib/controller/rewared_controller.dart
@@ -0,0 +1,6 @@
+import 'package:get/get.dart';
+
+class RewardedController extends GetxController {
+  var isRewarded = false.obs;
+  var isloaded = false.obs;
+}
--- lib/widgets/banner_ad_container.dart
+++ lib/widgets/banner_ad_container.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
 
+import 'package:coronampel/controller/rewared_controller.dart';
 import 'package:firebase_admob/firebase_admob.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 class BannerAdContainer extends StatefulWidget {
@@ -10,6 +12,7 @@ class BannerAdContainer extends StatefulWidget {
 }
 
 class _BannerAdContainerState extends State<BannerAdContainer> {
+  RewardedController rewardedController = RewardedController();
   MobileAdTargetingInfo targetingInfo = MobileAdTargetingInfo(
     keywords: <String>[
       'Coburg',
@@ -20,18 +23,6 @@ class _BannerAdContainerState extends State<BannerAdContainer> {
     childDirected: false,
   );
 
-  BannerAd createBannerAd() {
-    return BannerAd(
-      adUnitId: BannerAd.testAdUnitId,
-      size: AdSize.banner,
-      targetingInfo: targetingInfo,
-      listener: (MobileAdEvent event) {
-        print("BannerAd event $event");
-      },
-    );
-  }
-
-  BannerAd _bannerAd;
   @override
   void initState() {
     super.initState();
@@ -39,41 +30,62 @@ class _BannerAdContainerState extends State<BannerAdContainer> {
         (RewardedVideoAdEvent event, {String rewardType, int rewardAmount}) {
       print("RewardedVideoAd event $event");
       if (event == RewardedVideoAdEvent.rewarded) {
-        Get.snackbar('Rewarded', 'Jetzt gibts eine Belohnung');
         print('rewarded');
+        rewardedController.isRewarded.value = true;
+      }
+      if (event == RewardedVideoAdEvent.loaded) {
+        rewardedController.isloaded.value = true;
+      }
+      if (event == RewardedVideoAdEvent.closed) {
+        if (rewardedController.isRewarded.value == false) {
+          Get.snackbar('Feature nicht freigeschalten',
+              'Du hast Leider keinen Belohnung erhalten.');
+          rewardedController.isloaded.value = false;
+          RewardedVideoAd.instance.load(
+            adUnitId: RewardedVideoAd.testAdUnitId,
+            targetingInfo: targetingInfo,
+          );
+        }
+      }
+      if (event == RewardedVideoAdEvent.failedToLoad) {
+        print('left');
+        Get.snackbar('Feature nicht freigeschalten',
+            'Du hast Leider keinen Belohnung erhalten.');
+        RewardedVideoAd.instance.load(
+          adUnitId: RewardedVideoAd.testAdUnitId,
+          targetingInfo: targetingInfo,
+        );
       }
     };
     RewardedVideoAd.instance.load(
-        adUnitId: RewardedVideoAd.testAdUnitId, targetingInfo: targetingInfo);
-  }
-
-  @override
-  void dispose() {
-    _bannerAd?.dispose();
-    super.dispose();
+      adUnitId: RewardedVideoAd.testAdUnitId,
+      targetingInfo: targetingInfo,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    Timer(Duration(seconds: 1), () {
-      _bannerAd
-        ..load()
-        ..show(
-          // Positions the banner ad 60 pixels from the bottom of the screen
-          anchorOffset: 60.0,
-          // Positions the banner ad 10 pixels from the center of the screen to the right
-          horizontalCenterOffset: 10.0,
-          // Banner Position
-          anchorType: AnchorType.bottom,
-        );
-    });
-
     return Container(
-      child: ElevatedButton(
-        child: const Text('SHOW REWARDED VIDEO'),
-        onPressed: () {
-          RewardedVideoAd.instance.show();
-        },
+      child: Obx(
+        () => rewardedController.isRewarded.value
+            ? Container(
+                child: Text('Feature freigeschalten ✅'),
+              )
+            : rewardedController.isloaded.value
+                ? ElevatedButton(
+                    child: const Text('SHOW REWARDED VIDEO'),
+                    onPressed: () async {
+                      try {
+                        await RewardedVideoAd.instance.show();
+                        rewardedController.isloaded.value = false;
+                      } on PlatformException catch (e) {
+                        print(e.message);
+                      }
+                    },
+                  )
+                : Center(
+                    child: CircularProgressIndicator(),
+                  ),
       ),
     );
   }
